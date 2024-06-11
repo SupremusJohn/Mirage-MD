65817246118a2d51ba9094a1540f813660384e5b
Joygoodboy <joybotboy@gmail.com>
2024-06-11 17:20:20 +0000

    Update menu.js

--- commandes/menu.js
+++ commandes/menu.js
@@ -45,7 +45,15 @@ const date = moment().format('DD/MM/YYYY');
 │   *Country* :Ghana
 │.  *Release* *date* :2024
 │.  *Religion* : *Christianity*
-╰─────✧WA-BOT✧─────◆ \n\n`;
+│   *Uptime:* ${uptime}
+│   *Database:* ${rtotalreg} of ${totaluser} 
+│   *Total Users:* ${totaluser}
+    *Bot Name* : {*Mirage md*  v1.5.2}
+│        ────────
+│       * _User Info:_ *
+│    *User Tag:* ${taguser} 
+│    *Name:* ${name}  
+╰─────✧𝐌𝐈𝐑𝐀𝐆𝐄 𝐌𝐃 𝐕1.5.2-𝐁𝐎𝐓✧─────◆ \n\n`;
     
 let menuMsg = `
 👋 Hello ${nomAuteurMessage} 👋
